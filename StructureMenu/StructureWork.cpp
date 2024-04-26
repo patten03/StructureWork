002547e601e23412214b9d3f3672219773aa0fa9
patten03 <patten010203@gmail.com>
2024-04-26 23:06:08 +0300

    added structure

--- StructureMenu/StructureWork.cpp
+++ StructureMenu/StructureWork.cpp
@@ -6,5 +6,44 @@ bool receiveISS(const std::string& filename) {
 }
 
 void menu() {
-	std::cout << findFile("Вот такой у меня красивый заголовок", receiveISS);
+	std::cout << "Добро пожаловать в программу StructureWork." << std::endl
+		<< "Программа работает с учебными дисциплинами и их методами оценивания." << std::endl << std::endl
+		<< "Для работы с файлами они должны находится в той же директории, что и программа." << std::endl << std::endl;
+
+	bool quit(false); // переменная выхода из программы
+	while (!quit) {
+		std::cout << "Выберите действие:" << std::endl;
+		std::vector<std::string> menuPanel{
+			"Создать файл",
+			"Редактировать файл",
+			"Выйти из программы"
+		};
+		ask(menuPanel); // вывод действий в консоль для их выбора пользователем
+		int choice = inputChoice(menuPanel.size());
+
+		std::string file;
+		// реакция на выбор действия пользователем
+		switch (choice) {
+		case 1:
+		{ // создание файла
+
+			break;
+		}
+		case 2:
+		{ // редактирование файла
+			file = findFile("Выберите файл:", receiveISS);
+			if (file != "") {
+				continue;
+			}
+			break;
+		}
+		case 3:
+		{
+			quit = true;
+			break;
+		}
+		default:
+			break;
+		}
+	}
 }
--- StructureMenu/StructureWork.h
+++ StructureMenu/StructureWork.h
@@ -3,6 +3,19 @@
 
 #include "MenuWork.h"
 
+struct AddElm {
+	std::string session;
+	AddElm* ptr;
+};
+
+struct MainElm {
+	std::string book;
+	MainElm* ptr1; // ptrDown
+	AddElm* ptr2;  // ptrLeft
+};
+
+MainElm* main_ptr = nullptr;
+
 void menu();
 bool receiveISS(const std::string& filename);
 
